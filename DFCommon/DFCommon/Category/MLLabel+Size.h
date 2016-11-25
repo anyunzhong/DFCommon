1eede444aa8679e929603c73e4371b03dcdd249b
AllenZhong <2642754767@qq.com>
2016-11-25 13:41:18 +0800

    cocoa pod错误修正

--- DFCommon/DFCommon/Category/MLLabel+Size.h
+++ DFCommon/DFCommon/Category/MLLabel+Size.h
@@ -14,7 +14,6 @@
 
 +(CGSize) getViewSize:(NSAttributedString *)attributedText maxWidth:(CGFloat) maxWidth font:(UIFont *) font lineHeight:(CGFloat) lineHeight lines:(NSUInteger)lines;
 
-
 +(CGSize) getViewSizeByString:(NSString *)text maxWidth:(CGFloat) maxWidth font:(UIFont *) font lineHeight:(CGFloat) lineHeight lines:(NSUInteger)lines;
 
 +(CGSize) getViewSizeByString:(NSString *)text maxWidth:(CGFloat) maxWidth font:(UIFont *) font;
